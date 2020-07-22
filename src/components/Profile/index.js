db738e37f5ce7ae6cc04f7da8fe795e2ba5dce47
Anderson Meira <anderson.meira@gmail.com>
2020-07-21 23:05:03 -0300

    refactor profile queries to hooks

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query MySiteMetadata {
-        site {
-          siteMetadata {
-            description
-            title
-            position
-          }
+const Profile = () => {
+  const query = graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          description
+          title
+          position
         }
       }
-    `}
+    }
+  `
 
-    render={data => (
-      <div className="profile-wrapper">
-        <h1>{data.site.siteMetadata.title}</h1>
-        <h2>{data.site.siteMetadata.position}</h2>
-        <p>{data.site.siteMetadata.description}</p>
-      </div>
-    )} 
-  />
-)
+  const { site: { siteMetadata: { title, position, description }}} = useStaticQuery(query)
+
+  return (
+    <div className="profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile
